7066f9424b06eddf906f05fd05dac10445ebba28
RANVEER SINGH <ranveersingh@RANVEERs-MacBook-Pro.local>
2025-01-08 16:35:50 +0530

    bookings endpoint added

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ connectDB();
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
+app.use("/api/bookings", require("./routes/booking"));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
